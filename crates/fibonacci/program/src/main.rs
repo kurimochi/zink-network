f59711ee97458274917b41664e82d0a2f6f8cf0b
Kurimochi <kurimochi.sec@gmail.com>
2025-09-26 00:56:05 +0000

    docs: Add comments for demonstrate

--- crates/fibonacci/program/src/main.rs
+++ crates/fibonacci/program/src/main.rs
@@ -5,9 +5,9 @@ use fibonacci_program::fibonacci;
 use sp1_zkvm::io;
 
 pub fn main() {
-    let n: u32 = io::read();
+    let n: u32 = io::read(); // 入力を読み込む
 
-    let fib = fibonacci(n);
+    let fib = fibonacci(n); // フィボナッチ数列の第n項を計算
 
-    io::commit(&(fib, n));
+    io::commit(&(fib, n)); // commitの引数に関して証明が行われる
 }
